c68a4d4b72d25b70dbea29fe9cb99be02d799d3b
JPlayTwitch <80308681+JPlayTwitch@users.noreply.github.com>
2021-03-09 17:32:45 +0000

    Soldier advance & flee

--- Enemies/Soldier.gd
+++ Enemies/Soldier.gd
@@ -8,6 +8,53 @@ export(int) var dmg = 5
 export(int) var weighting = 3
 export(int) var vision = 10
 export(int) var evasion = 5
+export(int) var attack_range = 5
 
-func act(game,tile):
-	pass
+func act(game,me):
+	if self.visible:
+		var self_point = game.enemy_pathfinding.get_closest_point(Vector3(me.tile.x,me.tile.y,0))
+		var player_point = game.enemy_pathfinding.get_closest_point(Vector3(game.player_tile.x, game.player_tile.y, 0))
+		var path = game.enemy_pathfinding.get_point_path(self_point, player_point)
+		var attacking = false
+		if path:
+			assert(path.size() > 1)
+			var move_tile = Vector2(path[1].x, path[1].y)
+			# if they are out of range
+			if path.size()>attack_range:
+				var blocked = false
+				for enemy in game.enemies:
+					if enemy.tile == move_tile:
+						blocked = true
+						break
+				if !blocked:
+					me.tile = move_tile
+			# if you are next to the player
+			elif move_tile == game.player_tile:
+				# find somewhere to run directly opposite the player
+				var flee_tile = 2*Vector2(me.tile.x,me.tile.y)-Vector2(game.player_tile.x, game.player_tile.y)
+				# only allow enemy to flee if this is an unoccupied floor tile
+				var blocked = false
+				if game.map[flee_tile.x][flee_tile.y] == game.tile_floor:
+					for enemy in game.enemies:
+						if enemy.tile == flee_tile:
+							blocked = true
+							break
+				else:
+					blocked = true
+				if !blocked:
+					me.tile = flee_tile
+				if blocked:
+					attacking = true
+			else:
+				attacking = true
+#
+#				game.damage_player(dmg)
+#				me.sprite_node.frame = 1
+#				var t = Timer.new()
+#				t.set_wait_time(0.2)
+#				t.set_one_shot(true)
+#				self.add_child(t)
+#				t.start()
+#				yield(t, "timeout")
+#				t.queue_free()
+#				me.sprite_node.frame = 0
